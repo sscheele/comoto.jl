0dbc65859ff132124c5bb09fe6bc65716596f951
Sam <scheele@gatech.edu>
2021-03-22 16:45:08 -0400

    Add pure legibility test

--- src/comoto.jl
+++ src/comoto.jl
@@ -8,8 +8,14 @@ using Altro;
 import ForwardDiff;
 import CSV;
 using Dates;
+
+include("ros_interface.jl")
+
 const RBD = RigidBodyDynamics;
 const TO = TrajectoryOptimization;
+
+const OBJECT_SET = SArray{Tuple{3,2}}(reshape([0.5,0.,-0.17, 0.8, 0., -0.17], (3,2)));
+const OBJECT_POS = OBJECT_SET[:,1];
 
 function get_kuka_joint(kuka::Mechanism, jointname::String)
     ee_body = findbody(kuka, jointname)
@@ -357,6 +363,67 @@ function get_nominal_traj(start::AbstractVector, goal::AbstractVector, n_timeste
     SMatrix{length(start), n_timesteps}(ret_val)
 end
 
+function confirm_display_traj(solver::ALTROSolver, dt::Float64)
+    println("Ready to move to start?")
+    readline(stdin)
+    move_to(TO.states(solver)[1], 4.0)
+    println("Ready to dispatch?")
+    readline(stdin)
+    dispatch_trajectory(hcat(TO.states(solver)...), dt, 0.)
+end
+
+function test_pure_legibility()
+    model = Kuka(0)
+    # size(model)
+
+    kuka_tree = parse_urdf("kuka.urdf",remove_fixed_tree_joints=false)
+    end_effector_fn = get_kuka_ee_postition_fun(kuka_tree);
+
+    ctrl_dims, state_dims, dt = 7, 7, 0.25
+    n_timesteps = 20
+    joint_target = @SVector [-0.3902233335085379, 1.7501020413442578, 0.8403277122861033, -0.22924505085794067, 2.8506926562622024, -1.417026666483551, -0.35668663982214976] #far reaching case
+    joint_start = @SVector [-0.7240388673767146, -0.34790398102066433, 2.8303899987665897, -2.54032606205873, 1.3329587647643253, 2.7596249683074614, 0.850582268802067]
+
+    leg_costs = get_legibility_costs(ctrl_dims, state_dims, dt, n_timesteps, OBJECT_SET, joint_start, end_effector_fn);
+    vel_costs = get_jointvel_costs(ctrl_dims, state_dims, n_timesteps);
+
+    tf = (n_timesteps-1)*dt;
+    ctrl_linear = (joint_target - joint_start)/tf;
+    U0 = [ctrl_linear for _ in 1:(n_timesteps-1)];
+
+    cons = TO.ConstraintList(ctrl_dims, state_dims, n_timesteps);
+    add_constraint!(cons, TO.GoalConstraint(joint_target), n_timesteps);
+    add_constraint!(cons, TO.BoundConstraint(ctrl_dims, state_dims, u_min=-10, u_max=10), 1:n_timesteps-1);
+    # cannot constrain final timestep twice
+    add_constraint!(cons, TO.BoundConstraint(ctrl_dims, state_dims, x_min=-2π, x_max=2π), 1:n_timesteps-1);
+    
+    opts = SolverOptions(
+        penalty_scaling=10.,
+        active_set_tolerance_pn=0.01,
+        verbose_pn=true,
+        iterations_inner=60,
+        iterations_outer=15,
+        penalty_initial=0.1,
+        verbose=1,
+    )
+
+    while true
+        println("Enter leg weight")
+        leg_weight = parse(Float64, readline(stdin))
+        println("Enter velocity weight")
+        vel_weight = parse(Float64, readline(stdin))
+        weights = @SVector [leg_weight, vel_weight];
+        final_costs = [CompoundCost([leg_costs[i], vel_costs[i]], leg_costs[i].is_terminal, ctrl_dims, state_dims, weights) for i=1:n_timesteps];
+        obj = TO.Objective(final_costs);
+        prob = TO.Problem(model, obj, joint_target, tf, x0=joint_start, constraints=cons);
+        initial_controls!(prob, U0);
+
+        solver = ALTROSolver(prob, opts);
+        solve!(solver);
+        confirm_display_traj(solver, dt)
+    end
+end
+
 function main()
     model = Kuka(0)
     # size(model)
@@ -373,8 +440,6 @@ function main()
     dt = 0.25;
     n_timesteps = 20;
     N_HUMAN_JOINTS = 11;
-    OBJECT_SET = SArray{Tuple{3,2}}(reshape([0.5,0.,-0.17, 0.6, 0., -0.17], (3,2)));
-    OBJECT_POS = OBJECT_SET[:,1];
     
     # read in human trajectory means
     means_reader = CSV.File("means.csv");
@@ -467,15 +532,15 @@ function main()
     println("Violated joint constraints: ", any(x->any(y->y<-2π||y>2π, x), TO.states(solver)))
     println("Violated control constraints: ", any(x->any(y->y<-5||y>5, x), TO.controls(solver)))
     println("Reaches goal: ", sq_norm(joint_target - TO.states(solver)[end]) < 0.01)
-    println(TO.states(solver))
 
-    # for _ = 1:10
+    for _ = 1:10
         t_start = Dates.now()
         newsolver = ALTROSolver(prob, opts);
         solve!(newsolver)
         println("Time to solve: ", Dates.now() - t_start)
         println(TO.states(newsolver))
-    # end
+    end
 end
 
-main()
+# main()
+test_pure_legibility()
